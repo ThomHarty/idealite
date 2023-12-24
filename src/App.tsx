1f7dc946b7f426acfb327595eebcd97974799469
Thom Harty <thomharty@BAI-ThomasHarty.local>
2023-12-24 19:40:06 +0000

    Added Toggle Auth By Shaking Device

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import React, {ReactElement} from 'react';
+import React, {ReactElement, useEffect, useState} from 'react';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import RNShake from 'react-native-shake';
 
 import {COLORS, ThemeProvider} from './theme';
 import {OnboardingNavigation} from './navigation/OnboardingNavigation';
 import {TabNavigation} from './navigation/TabNavigation';
 
 export const App = (): ReactElement => {
-  const authed = false;
+  const [authed, setAuthed] = useState(false);
+
+  useEffect(() => {
+    const subscription = RNShake.addListener(() => {
+      setAuthed(current => !current);
+    });
+
+    return () => {
+      subscription.remove()
+    }
+  }, [])
 
   return (
     <GestureHandlerRootView
